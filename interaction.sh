bdc86e0f9d01a6ec09af21fa87eb029d88fba469
Emmanuel Frécon <efrecon@gmail.com>
2021-06-16 15:12:26 +0200

    fix bugs and add tests

--- interaction.sh
+++ interaction.sh
@@ -13,37 +13,48 @@ fi
 if ! printf %s\\n "${MG_MODULES:-}"|grep -q "options"; then
   printf %s\\n "This module requires the options module" >&2
 fi
+if ! printf %s\\n "${MG_MODULES:-}"|grep -q "controls"; then
+  printf %s\\n "This module requires the controls module" >&2
+fi
 if ! printf %s\\n "${MG_MODULES:-}"|grep -q "portability"; then
   printf %s\\n "This module requires the portability module" >&2
 fi
 
 prompt() {
-  stack_let varname
+  stack_let destination=
   stack_let force
   stack_let hidden
+  stack_let begin
+  stack_let question=
 
   parseopts \
     --options \
-      v,var,variable OPTION varname "" "Name of variable where to place result" \
+      v,var,variable,varname OPTION destination - "Name of variable where to place result" \
       f,force FLAG force 0 "When given, this flag forces the prompt to happen even if the variable is already set" \
-      h,hidden FLAG hidden 0 "Do not show what is input on screen (for secret information such as passwords)"
+      h,hidden FLAG hidden 0 "Do not show what is input on screen (for secret information such as passwords)" \
       help FLAG @HELP - "Print this help" \
+    --shift _begin \
     -- "$@"
 
+  # shellcheck disable=SC2154  # Var is set by parseopts
+  shift "$_begin"
+
+  [ "$#" -gt 0 ] && question="${1% } "
   # shellcheck disable=SC2154 # hidden is set via the options
-  if [ -n "$varname" ]; then
+  if [ -n "$destination" ]; then
     # shellcheck disable=SC2154 # force is set via the options
-    if var_empty "$varname" || [ "$force" = "1" ]; then
-      printf %s "${2% } "
+    if var_empty "$destination" || [ "$force" = "1" ]; then
+      printf %s "${question}"
       if [ "$hidden" = "1" ]; then
-        read_s -r "$1"
+        read_s -r "$destination"
       else
-        read -r "$1"
+        # shellcheck disable=SC2229  # Yes! We actually want the result in the var which name was passed as an option
+        read -r "$destination"
       fi
     fi
   else
     stack_let value
-    printf %s "${2% } "
+    printf %s "${question}"
     if [ "$hidden" = "1" ]; then
       read_s -r value
     else
@@ -53,5 +64,5 @@ prompt() {
     stack_unlet value
   fi
 
-  stack_unlet varname force
+  stack_unlet destination force begin question
 }
--- spec/interaction_spec.sh
+++ spec/interaction_spec.sh
@@ -2,7 +2,55 @@
 
 Describe 'interaction.sh'
   Include locals.sh
-  Include portability.sh
+  Include log.sh
   Include options.sh
+  Include controls.sh
+  Include filesystem.sh
+  Include portability.sh
   Include interaction.sh
+
+  Describe 'prompt'
+    askvar() {
+      _value=$1
+      shift
+      prompt --varname QUESTION "$@" -- "This is a question" >/dev/null <<EOF
+$(printf %s\\n "$_value")
+EOF
+      printf %s\\n "$QUESTION"
+    }
+    asknovar() {
+      prompt "This is a question" <<EOF
+$(printf %s\\n "$1")
+EOF
+    }
+
+    unset QUESTION || true
+    It "Sets the variable when it does not exist"
+      When call askvar "test"
+      The output should eq "test"
+    End
+
+    QUESTION=
+    It "Sets the variable when it is empty"
+      When call askvar "test"
+      The output should eq "test"
+    End
+
+    QUESTION="do not touch"
+    It "Does not set the variable when it exists"
+      When call askvar "test"
+      The output should eq "do not touch"
+    End
+
+    QUESTION="do not touch"
+    It "Sets the variable when forced to"
+      When call askvar "test" -f
+      The output should eq "test"
+    End
+
+    It "Returns the value and the prompt without variable"
+      When call asknovar "test"
+      The output should eq "This is a question test"
+    End
+  End
 End
